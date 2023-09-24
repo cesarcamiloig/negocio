6a778aba1f03c50daabc531830e1b589ccfc75d4
cesar <cesarcamiloig@gmail.com>
2023-09-24 12:23:50 -0500

    Correccion del metodo "withdraw" de la clase "CurrentAccount".

--- src/Modelo/CurrentAccount.java
+++ src/Modelo/CurrentAccount.java
@@ -15,7 +15,43 @@ public class CurrentAccount extends Account{
 	public void withdraw (double sum) 
 	{
 		
+		if (this.getBalance() >= 0) 
+		{
+			
+			if ((this.getBalance() + this.overdraftLimit) - sum >= 0) 
+			{
+				this.overdraftLimit -= this.getBalance() - sum < 0 ? this.getBalance() - sum: 0;
+				super.withdraw(sum);
+				
+				
+			}else 
+			
+			{
+				
+				System.err.println("Account.withdraw(...): " + "The withdrawal exceeds the current balance plus the overdraft limit");
+				
+			}
+			
+			
+		}else 
 		
+		{
+			
+			if (this.overdraftLimit - sum >= 0)
+			{
+				
+				this.overdraftLimit -= sum;
+				super.withdraw(sum);
+				
+			}else 
+				
+			{
+				
+				System.err.println("Account.withdraw(...): " + "Withdrawal exceeds overdraft limit");
+				
+			}	
+			
+		}
 		
 	}
 
